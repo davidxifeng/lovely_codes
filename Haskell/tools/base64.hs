16b191935c47669cd4c6e4fdb5bd50229adf14ba
david feng <davidxifeng@gmail.com>
2012-12-01 23:55:57 +0800

    recall haskell's import

--- Haskell/tools/base64.hs
+++ Haskell/tools/base64.hs
@@ -1,6 +1,11 @@
-import Data.ByteString as BS
-import Data.ByteString.UTF8 as BS.Utf8
-import Data.ByteString.Base64 as BS.B64
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.UTF8 as BS.Utf8
+import qualified Data.ByteString.Base64 as BS.B64
+{--
+qualified: 不可以使用没有加限定符的名字;
+as: 重命名;
+hiding: 可以隐藏掉某个符号;
+--}
 
 test = do
 	let s = "你好世界,永生门,TopGame3"

0a9fa15a769148c310f39b87710871b8b5bb9c8e
david feng <davidxifeng@gmail.com>
2012-12-18 23:04:52 +0800

    add shell utility usage example

---
+++ zsh/lily.sh
@@ -0,0 +1,15 @@
+#这个文件用来保存一些好用的工具, 或者一些命令的用法示例,等等
+sudo ngrep  -l -q -d eth0 "^GET | ^POST " tcp and port 80
+
+# Tue Dec 18 21:49:30 HKT 2012
+# 梦幻般的工具:do the right extraction
+dtrx /tmp/test.tgz
+...
+
+netcat
+
+mtr
+lftp
+
+#控制台版的irc client
+irssi

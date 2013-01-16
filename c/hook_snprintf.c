5657ca081377ec9853f698b78cf1a6f2f65960bd
david feng <davidxifeng@gmail.com>
2013-01-16 09:42:46 +0800

    add a new wrapper function, and verify a bug which actually is not a bug

--- c/hook_snprintf.c
+++ c/hook_snprintf.c
@@ -6,53 +6,100 @@
 #include <malloc.h> //malloc free ...
 #include <string.h> //strlen
 
-
 #define _snprintf snprintf_hook
 
-// %I64d -> %ld
-//int snprintf(char *str, size_t size, const char *format, ...);
 /**
- * 要求format为有效的格式化字符串,不能为null
+ * 性质:pure
+ * 替换字符串函数内部使用的专用函数,替换目标字符串中识别出的模式为预设内容
+ * %I64d -> %ld
  */
-int snprintf_hook(char *str, size_t size, const char *format, ...) {
+static
+void str_process(const char * src, char * dst){
+    while (*src != '\0')
+    {
+        switch (*src)
+        {
+        case '%':
+            *dst++ = *src++;
+            if (*src == 'I' && *(src + 1) == '6' && *(src + 2) == '4'
+                && *(src + 3) == 'd')
+            {
+                *dst++ = 'l';
+                *dst++ = 'd';
+                src += 4;
+            }
+            break;
+        default:
+            *dst++ = *src++;
+            break;
+        }
+    }
+}
+
+/**
+ * 功能:
+ * 实现函数
+ * int snprintf(char *str, size_t size, const char *format, ...);
+ * 中的格式化字符串转换,目前支持:
+ * %I64d -> %ld
+ *
+ * 要求:
+ * format为有效的格式化字符串,不能为null
+ */
+int snprintf_hook(char *str, size_t size, const char *format, ...)
+{
     int r;
     size_t fslen = strlen(format);
     char * real_fs = malloc(fslen + 1);
 
-    const char * p = format;
-    char * dst_cur = real_fs;
-    printf("input string is %s", format);
-    while( *p != '\0') {
-        switch (*p){
-        case '%':
-            *dst_cur++ = *p++;
-            if(*p == 'I' && *(p + 1) == '6' && *(p + 2) == '4' && *(p + 3) == 'd'){
-                *dst_cur++ = 'l';
-                *dst_cur++ = 'd';
-                p+= 4;
-            }
-            break;
-        default:
-            *dst_cur++ = *p++;
-            break;
-        }
-    }
-    printf("output string is %s", real_fs);
+    if (real_fs == NULL)
+        return -1; //原函数要求失败返回negative value
+
+    str_process(format, real_fs);
     va_list ap;
     va_start(ap, format);
-    vsnprintf(str, size, real_fs, ap);
+    r = vsnprintf(str, size, real_fs, ap);
+    va_end(ap);
+    free(real_fs);
+    return r;
+}
+/**
+ * 功能:
+ * 实现函数
+ * int sprintf(char *str, const char *format, ...);
+ * 中的格式化字符串转换,目前支持:
+ * %I64d -> %ld
+ *
+ * 要求:
+ * format为有效的格式化字符串,不能为null
+*/
+int sprintf_hook(char *str, const char *format, ...){
+    int r;
+    size_t fslen = strlen(format);
+    char * real_fs = malloc(fslen + 1);
+
+    if (real_fs == NULL)
+        return -1; //原函数也要求失败返回negative value ;-)
+
+    str_process(format, real_fs);
+    va_list ap;
+    va_start(ap, format);
+    r = vsprintf(str, real_fs, ap);
     va_end(ap);
     free(real_fs);
     return r;
 }
 
-void test(void){
+void test(void)
+{
     char str_buf[128];
     snprintf(str_buf, 128, "int %d, long int %ld, char %c\n", 23, 214748364922L,
-            'L');
+        'L');
     puts(str_buf);
-    snprintf_hook(str_buf, 128, "int %d, long int %I64d, char %c\n", 23, 214748364922L,
-            'L');
+    snprintf_hook(str_buf, 128, "int %d, long int %I64d, char %c\n", 23,
+        214748364922L, 'L');
+    puts(str_buf);
+    sprintf_hook(str_buf, "%%%I64d,%s.", 1234567890123456L,"S.H.E");
     puts(str_buf);
 }
 

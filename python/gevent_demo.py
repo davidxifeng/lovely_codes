95f1399a14577bf3f9704c823587474018e5acc0
david feng <davidxifeng@gmail.com>
2013-03-23 13:06:10 +0800

    add some python demo

---
+++ python/gevent_demo.py
@@ -0,0 +1,54 @@
+import time
+import gevent
+from gevent import select
+
+def foo():
+    print('Running in foo')
+    gevent.sleep(0)
+    print('Explicit context switch to foo again')
+
+def bar():
+    print('Explicit context to bar')
+    gevent.sleep(0)
+    print('Implicit context switch back to bar')
+
+def test1():
+	gevent.joinall([gevent.spawn(foo), gevent.spawn(bar),])
+
+
+start = time.time()
+tic = lambda: 'at %1.1f seconds' % (time.time() - start)
+
+def gr1():
+    # Busy waits for a second, but we don't want to stick around...
+    print('Started Polling: 1', tic())
+    select.select([], [], [], 2)
+    print('Ended Polling 1: ', tic())
+
+def gr2():
+    # Busy waits for a second, but we don't want to stick around...
+    print('Started Polling 2: ', tic())
+    select.select([], [], [], 2)
+    print('Ended Polling 2: ', tic())
+
+def gr3():
+    print("Hey lets do some stuff while the greenlets poll, at", tic())
+    gevent.sleep(1)
+
+
+
+def test_io_block():
+	gevent.joinall([
+		gevent.spawn(gr1),
+		gevent.spawn(gr2),
+		gevent.spawn(gr3),
+		])
+
+
+def main():
+	#test1()
+	test_io_block()
+
+
+if __name__ == "__main__":
+	main()
---
+++ python/zmq_message_demo.py
@@ -0,0 +1,20 @@
+"""The prompt part of a simply two process chat app."""
+import zmq
+
+def main(addr, who):
+
+    ctx = zmq.Context()
+    socket = ctx.socket(zmq.PUB)
+    socket.bind(addr)
+
+    while True:
+        msg = raw_input("%s> " % who)
+        socket.send_pyobj((msg, who))
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 3:
+        print "usage: prompt.py <address> <username>"
+        raise SystemExit
+    main(sys.argv[1], sys.argv[2])

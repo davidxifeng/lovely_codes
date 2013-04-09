7ed4bb18cbab4a2532c76c51c74122874e5c000c
david feng <davidxifeng@gmail.com>
2013-04-09 20:54:53 +0800

    更新一个3个优先级的算式求值的demo

---
+++ haskell/snippet/eval.hs
@@ -0,0 +1,53 @@
+-- 进行优先级比较的表达式求值函数
+-- Tue Apr  9 17:59:02 CST 2013
+
+
+type Expression = [Token]
+
+data Token = Plus | Minus | Times | Divided_By | Power | Number Double
+     deriving Show
+
+op_bp :: Token -> Int
+op_bp op =
+        case op of
+            Plus       -> 1
+            Minus      -> 1
+            Times      -> 2
+            Divided_By -> 2
+            Power      -> 3
+            otherwise  -> error "error input"
+
+eval' :: Expression -> Expression
+eval' [] = []
+eval' (c:[]) = [c]
+--eval (x:op:y:[]) = [cal op x y]
+eval' (x:op:y:[]) =
+        case op of
+            Plus       -> x:op:y:[]
+            Minus      -> x:op:y:[]
+            Times      -> [cal op x y]
+            Divided_By -> [cal op x y]
+            Power      -> [cal op x y]
+
+eval' (x:op:y:next_op:z:cs) =
+        if (op_bp op) >= (op_bp next_op)
+            then eval' $ (cal op x y) : next_op: z : cs
+            else eval' (x : op: (eval' $ y:next_op:z : cs))
+
+eval :: Expression -> Token
+eval exp = cal' $ eval' exp
+
+cal' :: Expression -> Token
+cal' (x:op:y:[]) = cal op x y
+cal' (x:[]) = x
+
+cal :: Token -> Token -> Token -> Token
+cal op (Number x) (Number y) =
+        case op of
+            Plus       -> Number $ x + y
+            Minus      -> Number $ x - y
+            Times      -> Number $ x * y
+            Divided_By -> Number $ x / y
+            Power      -> Number $ x ** y
+
+test = eval [Number 2, Plus , Number 3, Times , Number 4, Power, Number 2, Plus, Number 5, Times, Number 2]
--- python/unicode.py
+++ python/unicode.py
@@ -1,25 +1,27 @@
 #coding:utf-8
 
-import sys
-
-print sys.getdefaultencoding()
-
-# to use this function, set python with -S
-sys.setdefaultencoding('utf-8')
-
-print sys.getdefaultencoding()
-
-us = u'你好我爱你abc'
-print type(us)
-
+def test():
+    import sys
+    print sys.getdefaultencoding()
+# to use this function, start python with -S
+    sys.setdefaultencoding('utf-8')
+    print sys.getdefaultencoding()
+    us = u'你好我爱你abc'
+    print type(us)
 # print u'你好我爱你abc'
 # will UnicodeEncodeError: 'ascii' codec can't encode characters in
 # position 0-4: ordinal not in range(128)
+    utf8_str = us.encode('utf-8')
+    print utf8_str
+    us2 = utf8_str.decode('utf-8')
+    print type(us2)
+    assert type(us2) == type(us)
 
-utf8_str = us.encode('utf-8')
-print utf8_str
+def test_kanzi():
+    a = u'你'
+    print ord(a)
+    print ord(u'好')
+    print ord('3')
 
-us2 = utf8_str.decode('utf-8')
-print type(us2)
-
-assert type(us2) == type(us)
+if __name__ == '__main__':
+    test_kanzi()

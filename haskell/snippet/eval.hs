c184fef2c200034de2d1d71a234c586497fb278e
david feng <davidxifeng@gmail.com>
2013-05-24 15:43:58 +0800

    小的更新

--- haskell/snippet/eval.hs
+++ haskell/snippet/eval.hs
@@ -2,12 +2,16 @@ module Eval (Token(..), Expression, eval)
 where
 
 import Data.Char ( isSpace
+                 , isDigit
                  )
 
 import Text.Parsec ( ParseError, parse, many, many1, spaces
-                   , digit, char, eof, (<|>)
+                   , digit, char, eof, (<|>), satisfy
+                   , choice, try
                    )
+
 import Text.Parsec.String (Parser)
+import Text.Parsec.Token (float)
 -- Tue Apr  9 17:59:02 CST 2013 进行优先级比较的表达式求值函数
 -- Fri May 24 14:02:35 CST 2013 学习了Parsec库的使用,写了一个简单的解析器
 
@@ -20,37 +24,30 @@ data Token = Plus | Minus | Times | Divided_By | Power | Number !Double
 
 type Expression = [Token]
 
-op :: Char -> Parser Token
-op p = do
-        char p
-        spaces
-        return $ fp p
-    where
-        fp '-' = Minus
-        fp '+' = Plus
-        fp '*' = Times
-        fp '/' = Divided_By
-        fp '^' = Power -- 如果使用**表示power就需要look ahead了
-
-parse_number :: Parser Token
-parse_number = do
-        ds <- many1 digit
-        spaces
-        return $ Number (read ds :: Double)
+-- 解析2.3 或者2 这样简单的带小数点的数字
+parse_double :: Parser Token
+parse_double =
+        try (do
+            ds <- many1 digit
+            char '.'
+            f <- many1 digit
+            spaces
+            return $ Number (read (ds ++ '.' : f) :: Double)
+        )
+        <|>
+        (do
+            ds <- many1 digit
+            spaces
+            return $ Number (read ds :: Double)
+        )
 
 expr :: Parser Token
 expr =
-        op '+'
+        choice (map
+               (\(o,t)-> char o >> spaces >> return t)
+               (zip "+-*/^" [Plus, Minus, Times, Divided_By, Power]))
         <|>
-        op '-'
-        <|>
-        op '*'
-        <|>
-        op '/'
-        <|>
-        op '^'
-        <|>
-        parse_number
+        parse_double
 
 exprs :: Parser [Token]
 exprs = do
@@ -105,5 +102,5 @@ eval s =
         in
             r
 
-test = eval "12 + 2 *3 /4 +5^2 "
-test' = 12 + 2 *3 /4 +5**2
+test = eval "12.5 + 2 *3 /4 +5^2 "
+test' = 12.5 + 2 *3 /4 +5**2

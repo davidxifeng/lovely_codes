4eae20ecc768e22fdce7c0073b78d566e13b8bc2
david feng <davidxifeng@gmail.com>
2013-09-09 17:07:57 +0800

    添加之前测试persistent的教程demo代码

---
+++ haskell/snippet/library/persistent_demo.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+import           Control.Monad.IO.Class  (liftIO)
+import           Database.Persist.Postgresql
+import           Database.Persist.TH
+import Data.Maybe (fromJust)
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person
+    name String
+    age Int Maybe
+    deriving Show
+BlogPost
+    title String
+    authorId PersonId
+    age Int Maybe -- test migrate
+    deriving Show
+|]
+
+main :: IO ()
+main = do
+        withPostgresqlPool connStr 8 x
+        return ()
+    where
+        connStr = "host=localhost port=5432 dbname=postgres user=david password=dev"
+
+x :: ConnectionPool -> IO ()
+x = runSqlPersistMPool $ do
+    runMigration migrateAll
+
+    --johnId <- insert $ Person "John Doe" $ Just 35
+    r <- selectFirst [PersonAge ==. Just 33] []
+    liftIO $ print $ fromJust r
+    liftIO $ putStrLn "\n"
+
+    let johnId = entityKey $ fromJust r
+    oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
+    liftIO $ print (oneJohnPost :: [Entity BlogPost])
+    liftIO $ putStrLn "\n"
+    --let johnId = fromJust r
+    --janeId <- insert $ Person "Jane Doe" Nothing
+
+    --insert $ BlogPost "love you My first post" johnId
+    --insert $ BlogPost "One more for good measure" johnId
+
+    john <- get johnId
+    liftIO $ print (john :: Maybe Person)
+
+    --delete janeId
+    --deleteWhere [BlogPostAuthorId ==. johnId]
+

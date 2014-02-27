56bdf936c9ccccae65c407d462857ada8a2bdcb5
david feng <davidxifeng@gmail.com>
2014-02-27 23:27:56 +0800

    test luajit and lua

---
+++ lua/bytecode/learn_bytecode.lua
@@ -0,0 +1,15 @@
+
+EquipPos = {
+    PosHelmet = 1,
+    PosJacket = 2,
+    PosJewel  = 3,
+    PosRing   = 4,
+    PosWeapon = 5,
+    PosBadge  = 6,
+}
+
+td = {'hi', 'you'}
+
+print(td[EquipPos.PosHelmet])
+print(td[2])
+

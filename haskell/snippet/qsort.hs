4ef5a272e85b8b5ad265f8e510035fcac2e9f4ec
david feng <davidxifeng@gmail.com>
2014-02-27 23:29:09 +0800

    test with :main in GHCi

--- haskell/snippet/qsort.hs
+++ haskell/snippet/qsort.hs
@@ -1,5 +1,7 @@
 module Main (main) where
 
+import System.Environment
+
 qsort [] = []
 qsort (a:as) = qsort left ++ [a] ++ qsort right
     where (left, right) = (filter (<= a) as, filter (>a) as)
@@ -9,4 +11,5 @@ test = print (qsort [8, 4, 0, 3, 1, 23, 11, 18])
 
 main :: IO ()
 main = do
+    getArgs >>= print
     test

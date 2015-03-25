0defb5a32579cda2c43550c796fd622554f32a29
David Feng <davidxifeng@gmail.com>
2015-03-26 02:26:37 +0800

    wireworld: create world from image; bug fix

--- lua/web/wireworld.lua
+++ lua/web/wireworld.lua
@@ -1,73 +1,129 @@
-local function class()
-    local cls = {ctor = function() end}
-    cls.__index = cls
-    function cls.new(...)
-        local instance = setmetatable({}, cls)
-        instance:ctor(...)
-        return instance
+local is_debug = true
+
+local grid_len = 4
+local gap_len = 2
+
+local CellType = { Head = 1, Tail = 2, Conductor = 3 }
+
+local function get_cells_from_image(image_url, on_load)
+    local img = window.document:createElement('img')
+    img.src = image_url
+    img.onload = function ()
+        local canvas = window.document:createElement('canvas')
+        canvas.width = img.width
+        canvas.height = img.height
+        if is_debug then
+            window.document.body:appendChild(canvas)
+        end
+
+        local context = canvas:getContext('2d')
+        context:drawImage(img, 0, 0)
+
+
+        local imgData = context:getImageData(0, 0, img.width, img.height)
+        local iw, ih = imgData.width, imgData.height
+        local data = imgData.data
+        local cells = {}
+        for y = 0, ih - 1 do
+            for x = 0, iw - 1 do
+                local b = y * iw * 4 + x * 4
+                local r, g, b = data[b], data[b + 1], data[b + 2]
+
+                local cell_type
+                if r >= 128 and g >= 128 and b < 128 then
+                    cell_type = CellType.Conductor
+                elseif r >= 128 and g < 128 and b < 128 then
+                    cell_type = CellType.Tail
+                elseif r < 128 and g < 128 and b >= 128 then
+                    cell_type = CellType.Head
+                end
+
+                if cell_type then
+                    table.insert(cells, {
+                        x = x + 1, y = y + 1, type_ = cell_type
+                    })
+                end
+            end
+        end
+        if on_load then
+            on_load(iw, ih, cells)
+        end
     end
-    return cls
 end
 
-CellType = { Head = 1, Tail = 2, Conductor = 3 }
+local function create_world(image_url, world_loader)
 
-local function create_world(w, h, cells)
-
-    local function build_cells()
+    local function build_cells(cells)
         local r = {}
         for _, v in ipairs(cells) do
-            local row = r[v.x]
+            local row = r[v.y]
             if not row then
                 row = {}
-                r[v.x] = row
+                r[v.y] = row
             end
-            row[v.y] = v.type_
+            row[v.x] = v.type_
         end
         return r
     end
 
-    -- init grid info
-    local grid_len, gap_len = 50, 10
-    local cw, ch = (grid_len + gap_len) * w, (grid_len + gap_len) * h
-    local offset_x, offset_y = 400 - cw / 2, 300 - ch / 2
+    get_cells_from_image(image_url, function (w, h, cells)
 
-    return {
-        width    = w,
-        height   = h,
-        c_width  = cw,
-        c_height = ch,
-        offset_x = offset_x,
-        offset_y = offset_y,
-        is_a     = true,
-        cells_a  = build_cells(cells),
-        cells_b  = build_cells(cells),
-    }
+        -- init grid info
+        local cw, ch = (grid_len + gap_len) * w, (grid_len + gap_len) * h
+        local offset_x, offset_y = 400 - cw / 2, 300 - ch / 2
+
+        world_loader {
+            width    = w,
+            height   = h,
+            c_width  = cw,
+            c_height = ch,
+            offset_x = offset_x,
+            offset_y = offset_y,
+            is_a     = true,
+            cells_a  = build_cells(cells),
+            cells_b  = build_cells(cells),
+            gens     = 0,
+        }
+    end)
 end
 
 local function draw_world(context, world)
+    -- 迭代次数
+    local rc = function() return -30, -80, 55, 20 end
+    context:clearRect(rc())
+    if is_debug then
+        context.fillStyle = 'gray'
+        context:fillRect(rc())
+    end
+    context.fillStyle = 'red'
+    context:fillText(world.gens, -20, -65)
+
     local function draw_cell(j, k, cell_type)
-        local y, x = j * 60 - 55, k * 60 - 55
+        local cell_border = 0.5
+
+        local x = k * (grid_len + gap_len) - grid_len - gap_len / 2
+        local y = j * (grid_len + gap_len) - grid_len - gap_len / 2
+
+        -- draw cell border
         context.strokeStyle = 'black'
-        context.lineWidth = 3
+        context.lineWidth = cell_border
         context.lineCap = 'round'
         context.lineJoin = 'round'
-        context:strokeRect(x, y, 50, 50)
+        context:strokeRect(x, y, grid_len, grid_len)
 
         local fs
-        if cell_type == CellType.Conductor then
-            fs = 'yellow'
-        elseif cell_type == CellType.Head then
-            fs = 'blue'
-        elseif cell_type == CellType.Tail then
-            fs = 'red'
+        if     cell_type == CellType.Conductor then fs = 'yellow'
+        elseif cell_type == CellType.Head      then fs = 'blue'
+        elseif cell_type == CellType.Tail      then fs = 'red'
         end
 
+        -- draw cell rect
         context.fillStyle = fs
-        context:fillRect(x, y, 50, 50)
+        context:fillRect(x, y, grid_len, grid_len)
     end
 
+    -- cells
     context:clearRect(0, 0, world.c_width, world.c_height)
-
     local cells = world.is_a and world.cells_a or world.cells_b
     for j, row in pairs(cells) do
         for k, cell_type in pairs(row) do
@@ -78,22 +134,18 @@ end
 
 local function transition_world(world)
     local function check_neighbour_head(cells, j, k)
-        local j_m_1 = j - 1
-        local min_x = j_m_1 >= 1 and j_m_1 or 1
-        local j_p_1 = j + 1
-        local max_x = j_p_1 <= world.width and j_p_1 or world.width
-        local k_m_1 = k - 1
-        local min_y = k_m_1 >= 1 and k_m_1 or 1
-        local k_p_1 = k + 1
-        local max_y = k_p_1 <= world.height and k_p_1 or world.height
+        local max_y = j < world.height and j + 1 or world.height
+        local max_x = k < world.width and k + 1 or world.width
         local count = 0
-        for x = min_x, max_x do
-            for y = min_y, max_y do
-                local row = cells[x]
-                local cell = row and row[y]
-                if cell and cell == CellType.Head then
-                    count = count + 1
-                end
+        for y = j > 1 and j - 1 or 1, max_y do
+            for x = k > 1 and k - 1 or 1, max_x do
+                --if y ~= j or x ~= k then
+                    local row = cells[y]
+                    local cell = row and row[x]
+                    if cell and cell == CellType.Head then
+                        count = count + 1
+                    end
+                --end
             end
         end
         return count == 1 or count == 2
@@ -116,6 +168,7 @@ local function transition_world(world)
             end
         end
     end
+
     if world.is_a then
         world.is_a = false
         update_cells(world.cells_b, world.cells_a)
@@ -123,26 +176,13 @@ local function transition_world(world)
         world.is_a = true
         update_cells(world.cells_a, world.cells_b)
     end
+    world.gens = world.gens + 1
 end
 
 -- main
-do
-    local demo_cells = {
-        { x = 1, y = 2, type_ = CellType.Conductor},
-        { x = 1, y = 3, type_ = CellType.Head},
-        { x = 1, y = 4, type_ = CellType.Tail},
-        { x = 2, y = 1, type_ = CellType.Conductor},
-        { x = 3, y = 1, type_ = CellType.Conductor},
-        { x = 4, y = 1, type_ = CellType.Conductor},
-        { x = 2, y = 5, type_ = CellType.Conductor},
-        { x = 3, y = 5, type_ = CellType.Conductor},
-        { x = 4, y = 5, type_ = CellType.Conductor},
-        { x = 5, y = 2, type_ = CellType.Conductor},
-        { x = 5, y = 3, type_ = CellType.Conductor},
-        { x = 5, y = 4, type_ = CellType.Conductor},
-    }
-
-    local the_world = create_world(5, 5, demo_cells)
+local image_url = 'image/cycle.bmp'
+local image_url = 'image/circuit.bmp'
+create_world(image_url, function (the_world)
     local context = window.document:getElementById('ww'):getContext('2d')
     context:translate(the_world.offset_x, the_world.offset_y)
 
@@ -151,9 +191,9 @@ do
         transition_world(the_world)
     end
 
-    local debug = true
-    local fps = debug and 5 or 60
+    --local fps = is_debug and 2 or 60
+    local fps = 60
     game_loop()
     window:setInterval(game_loop, 1000 / fps)
     --window:setTimeout(game_loop, 1000 / fps)
-end
+end)

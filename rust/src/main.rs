ee66966bd7e2d5ecefdd80bd22fc54a88701a42d
David Feng <davidxifeng@gmail.com>
2022-02-11 22:43:57 +0900

    learn some rust for fun

---
+++ rust/src/main.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long, default_value = "dev")]
+    name: String,
+
+    /// Number of times to greet
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("Hello, {}", args.name);
+    println!("2022-2-11 wsl lunar neovim");
+}

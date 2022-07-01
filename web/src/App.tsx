a62ddda27a12fc663ae9b3cb35764f9514b009c5
David Feng <davidxifeng@gmail.com>
2022-07-02 04:04:03 +0900

    very simple dark mode switch

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 
 export default function () {
-  const [count, setCount] = useState(0)
+  const [dark, setDarkMode] = useState(true)
+  useEffect(() => {
+    if (dark || window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+
+  }, [dark])
 
   return (
     <>
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <button onClick={() => setDarkMode(!dark)}>{dark ? 'Dark' : 'Light'}</button>
+      <div className="dark:bg-black bg-white p-6 max-w-sm mx-auto rounded-xl shadow-lg flex items-center space-x-4">
         <div className="shrink-0">
           <img className="h-12 w-12" src={logo} alt="ChitChat Logo" />
         </div>
         <div>
-          <div className="text-xl font-medium text-black">ChitChat</div>
-          <p className="text-slate-500">You have a new message!</p>
+          <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
+          <p className="text-slate-500 dark:text-slate-100">You have a new message!</p>
         </div>
       </div>
     </>

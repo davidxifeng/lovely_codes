48e89d6431002f86a11adda1d4d28e2e8249c299
David Feng <davidxifeng@gmail.com>
2022-07-02 03:02:07 +0900

    a gif generation sample from book The Go Programming Language; Go's std lib is GREAT

--- go/hello.go
+++ go/hello.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
-    fmt.Println("Hello, go lang!")
+	rand.Seed(time.Now().UTC().UnixNano())
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			lissajous(w)
+		}
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	} else {
+		lissajous(os.Stdout)
+	}
 }
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.0001
+		size    = 100
+		nframes = 100
+		delay   = 2
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), 2)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+
+	}
+	gif.EncodeAll(out, &anim)
+}
+
+var palette = []color.Color{color.Black, color.White, color.RGBA{G: 255, A: 255}}

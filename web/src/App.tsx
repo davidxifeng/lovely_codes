04fc37e79e34fac751746f3431c7f4b047632ce2
David Feng <davidxifeng@gmail.com>
2022-07-02 01:54:33 +0900

    web playground using vite + react + tailwindcss

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+export default function () {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div className="shrink-0">
+          <img className="h-12 w-12" src={logo} alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
+    </>
+  )
+}
+

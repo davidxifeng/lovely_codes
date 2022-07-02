b26be3853d0a6a154561c5631d9ab6bf5d23afb5
David Feng <davidxifeng@gmail.com>
2022-07-02 23:37:58 +0900

    fix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,7 +14,8 @@ export default function () {
 
   return (
     <>
-      <button onClick={() => setDarkMode(!dark)}>{dark ? 'Dark' : 'Light'}</button>
+      <button className='px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2'
+        onClick={() => setDarkMode(!dark)}>{dark ? 'Dark' : 'Light'}</button>
       <div className="dark:bg-black bg-white p-6 max-w-sm mx-auto rounded-xl shadow-lg flex items-center space-x-4">
         <div className="shrink-0">
           <img className="h-12 w-12" src={logo} alt="ChitChat Logo" />
@@ -23,6 +24,10 @@ export default function () {
           <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
           <p className="text-slate-500 dark:text-slate-100">You have a new message!</p>
         </div>
+      </div>
+      <div className='container mx-auto px-4 p-3 my-3 dark:bg-blue-900 '>
+        <p className='dark:text-slate-100 text-slate-800'>this is a container description</p>
+
       </div>
     </>
   )

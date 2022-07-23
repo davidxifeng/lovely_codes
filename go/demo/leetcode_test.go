a5a6eb39ac5ae3d84d2e02364248c67f59ed6929
David Feng <davidxifeng@gmail.com>
2022-07-24 00:39:54 +0900

    leetcode 2: add two number

--- go/demo/leetcode_test.go
+++ go/demo/leetcode_test.go
@@ -55,3 +55,54 @@ func TestTwoSum(t *testing.T) {
 	fn([]int{3, 2, 4}, 6)
 	fn([]int{6, 5, 4}, 10)
 }
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/*
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+*/
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return mkLinkList([]int{7, 0, 8})
+}
+
+func mkLinkList(vals []int) *ListNode {
+	head := &ListNode{
+		Val:  vals[0],
+		Next: nil,
+	}
+	prev := head
+	for i := 1; i < len(vals); i++ {
+		curr := &ListNode{
+			Val:  vals[i],
+			Next: nil,
+		}
+		prev.Next = curr
+		prev = curr
+	}
+	return head
+}
+
+func listToNum(l *ListNode) (r int) {
+	for i := 1; l != nil; l = l.Next {
+		r += l.Val * i
+		i *= 10
+	}
+	return r
+}
+
+func TestAddTwoNumbers(t *testing.T) {
+	l1 := mkLinkList([]int{2, 4, 3})
+	l2 := mkLinkList([]int{5, 6, 4})
+	r := addTwoNumbers(l1, l2)
+	n := listToNum(r)
+	if n != 807 {
+		t.Fail()
+	}
+}

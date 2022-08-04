5c5f393c00bd7a07cddcc6d05e6ad55deedd6c2b
David Feng <davidxifeng@gmail.com>
2022-08-04 23:41:56 +0900

    learn rust: add notes

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -46,6 +46,40 @@ fn test_basic() {
 
 }
 
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
+impl Rect {
+    // 没有self参数的: 关联函数, 构造数据惯用new;
+    // 例子: String::from
+    fn empty() -> Rect {
+        Rect {
+            width: 1,
+            height: 1,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+
+// 函数之外,结构体,枚举,方法都可以使用泛型. 支持特化
+// const泛型 针对值的泛化,用来支持数组.
+// 不同长度的数组类型不一样
+// rust泛型没有运行时开销,一贯的零成本抽象; 编译时
+// 会根据用到的类型生成多份代码,会增加编译时间和目标
+// 代码size
+
+// 特征与类型: 至少有一个是在当前作用域定义的.
+// 这样可以避免修改无关的内容.
+
+// 特征对象 动态派发
+
+
 fn main() {
     let args = Args::parse();
 
@@ -57,4 +91,8 @@ fn main() {
 
     test_for_loop();
     test_basic();
+
+    let r = Rect::empty();
+    println!("r area: {}", r.area());
+
 }

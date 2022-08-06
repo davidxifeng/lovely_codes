724b32d8400111e26d0a7212da1baf1bf73a8436
David Feng <davidxifeng@gmail.com>
2022-08-06 11:18:28 +0900

    继续学习rust: 所有权与move语义,结构体,枚举. 简单场景下,rust根本一点就不难,传闻中的难度太夸张了.

--- rust/rustlings/exercises/enums/enums2.rs
+++ rust/rustlings/exercises/enums/enums2.rs
@@ -1,11 +1,15 @@
 // enums2.rs
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    // Golang中的痛点,Haskell语言爱好者对rust表示非常开心
+    // sum type
+    Move {x: i32, y: i32},
+    Echo(String),
+    ChangeColor(u8,u8,u8),
+    Quit,
 }
 
 impl Message {
